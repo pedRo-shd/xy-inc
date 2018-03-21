4f401e03e7e8ce38015fea95fa883b4349b608a3
Pedro Custódio <pedro@dogme.com.br>
2018-03-21 17:21:36 -0300

    allows any request HTTP

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,24 @@ module XyInc
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        headers: :any,
+        methods: %i(get post put patch delete options head)
+      end
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+
   end
 end
