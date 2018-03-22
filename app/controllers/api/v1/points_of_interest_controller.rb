acd034b9d9574ff1af8907b644bc5eef310c652d
Pedro Custódio <pedro@dogme.com.br>
2018-03-22 13:18:47 -0300

    Creating point of interest

--- app/controllers/api/v1/points_of_interest_controller.rb
+++ app/controllers/api/v1/points_of_interest_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::PointsOfInterestController < ApplicationController
+
+  def index
+    #code
+  end
+
+  def create
+    @point_of_interest = PointOfInterest.new(point_of_interest_params)
+
+    if @point_of_interest.save
+      render @point_of_interest, status: :created
+    else
+      render json: @point_of_interest.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def point_of_interest_params
+    params.require(:point_of_interest).permit(:name, :x, :y)
+  end
 end
--- spec/api/v1/points_of_interest_spec.rb
+++ spec/api/v1/points_of_interest_spec.rb
@@ -3,8 +3,60 @@ require 'rails_helper'
 RSpec.describe Api::V1::PointsOfInterestController, type: :controller do
   describe "POST #create" do
 
-    context "" do
-      before do
+    context "register point of interest" do
+      it "must have success status with allowed attributes" do
+        post :create, params: {
+           point_of_interest: {
+              name: 'Restaurant', x: 10, y: 25
+           }
+        }, format: :json
+
+        expect(response).to be_success
+        expect(response).to have_http_status(201)
+      end
+
+      it "mustn't have success status with attributes x negative" do
+        post :create, params: {
+           point_of_interest: {
+              name: 'Restaurant', x: -10, y: 25
+           }
+        }, format: :json
+
+        expect(response).not_to be_success
+        expect(response).to have_http_status(422)
+      end
+
+      it "mustn't have success status without attributes name" do
+        post :create, params: {
+           point_of_interest: {
+              name: '', x: -10, y: 25
+           }
+        }, format: :json
+
+        expect(response).not_to be_success
+        expect(response).to have_http_status(422)
+      end
+
+      it "mustn't have success status with attributes y negative" do
+        post :create, params: {
+           point_of_interest: {
+              name: '', x: 10, y: -25
+           }
+        }, format: :json
+
+        expect(response).not_to be_success
+        expect(response).to have_http_status(422)
+      end
+
+      it "mustn't have success status with invalid attributes params" do
+        post :create, params: {
+           point_of_interest: {
+              nameeeeeee: 'Restaurant', x_x: 10, y: 25
+           }
+        }, format: :json
+
+        expect(response).not_to be_success
+        expect(response).to have_http_status(422)
       end
     end
 
