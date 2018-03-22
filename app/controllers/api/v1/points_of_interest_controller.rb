5dfb4e3aeeb13719473856a0bdb58e963ab0b40b
Pedro Custódio <pedro@dogme.com.br>
2018-03-22 13:06:54 -0300

    File spec and controller points of interest

---
+++ app/controllers/api/v1/points_of_interest_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::PointsOfInterestController < ApplicationController
+end
---
+++ spec/api/v1/points_of_interest_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::PointsOfInterestController, type: :controller do
+  describe "POST #create" do
+
+    context "" do
+      before do
+      end
+    end
+
+  end
+end
