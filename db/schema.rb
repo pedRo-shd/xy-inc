5e4566c50b2d9979de83e517ec816b1c0b8b2851
Pedro Custódio <pedro@dogme.com.br>
2018-03-22 22:49:52 -0300

    Included database postgres to use geometric types

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180321212356) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "point_of_interests", force: :cascade do |t|
     t.string "name"
     t.integer "x"
