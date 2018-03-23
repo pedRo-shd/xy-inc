c376f50fb5d71a0846fc987c30e9445819f60239
Pedro Custódio <pedro@dogme.com.br>
2018-03-22 22:53:01 -0300

    Adjusting in rspec

--- spec/api/v1/points_of_interest_spec.rb
+++ spec/api/v1/points_of_interest_spec.rb
@@ -54,42 +54,19 @@ RSpec.describe Api::V1::PointsOfInterestController, type: :controller do
 
     context "listing all point of interest" do
       before do
-        @point_of_interest_1 = create(:point_of_interest, x: 20, y: 10)
-        @point_of_interest_2 = create(:point_of_interest, x: 10, y: 20)
-        @point_of_interest_3 = create(:point_of_interest, x: 50, y: 50)
-        @point_of_interest_4 = create(:point_of_interest, x: 5, y: 30)
-        @point_of_interest_5 = create(:point_of_interest, x: 100, y: 2)
-        @point_of_interest_5 = create(:point_of_interest, x: 20, y: 20)
+        @point_of_interest_1 = create(:point_of_interest, name: 'Lanchonete', x: 27, y: 12)
+        @point_of_interest_2 = create(:point_of_interest, name: 'Posto', x: 31, y: 18)
+        @point_of_interest_3 = create(:point_of_interest, name: 'Joalheria', x: 15, y: 12)
+        @point_of_interest_4 = create(:point_of_interest, name: 'Floricultura', x: 19, y: 21)
+        @point_of_interest_5 = create(:point_of_interest, name: 'Pub', x: 12, y: 8)
+        @point_of_interest_6 = create(:point_of_interest, name: 'Supermercado', x: 23, y: 6)
+        @point_of_interest_7 = create(:point_of_interest, name: 'Churras', x: 28, y: 2)
       end
 
-      it "must return 3 points of interest" do
+      it "must return 4 points of interest" do
         get :get_by_proximity, params: { x: 20, y: 10, d_max: 10 }
 
-        expect(JSON.parse(response.body).count).to eql(3)
-      end
-
-      it "mustn't return points of interest" do
-        get :get_by_proximity, params: { x: 1000, y: 1000, d_max: 20 }
-
-        expect(JSON.parse(response.body).count).to eql(0)
-      end
-
-      it "must return 1 point of interest" do
-        get :get_by_proximity, params: { x: 40, y: 40, d_max: 10 }
-
-        expect(JSON.parse(response.body).count).to eql(1)
-      end
-
-      it "must return 1 point of interest" do
-        get :get_by_proximity, params: { x: 99, y: 1, d_max: 1 }
-
-        expect(JSON.parse(response.body).count).to eql(1)
-      end
-
-      it "must return 1 point of interest" do
-        get :get_by_proximity, params: { x: 30, y: 30, d_max: 20 }
-
-        expect(JSON.parse(response.body).count).to eql(1)
+        expect(JSON.parse(response.body).count).to eql(4)
       end
     end
 
